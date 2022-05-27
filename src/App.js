3054d41519beb91fa97a974bb0fa93bcdaff8ebc
kate2797 <katka.zaprazna@icloud.com>
2022-05-27 20:50:39 +0100

    Added rolling dice MVP

--- src/App.js
+++ src/App.js
@@ -2,20 +2,32 @@ import React from "react";
 import Die from "./components/Die";
 
 export default function App() {
+  const [dice, setDice] = React.useState(allNewDice()); // initialise an array of random numbers
+
+  function allNewDice() {
+    const diceArray = [];
+    for (let i = 0; i < 10; i++) {
+      let randomNum = Math.floor(Math.random() * 6) + 1;
+      diceArray.push(randomNum);
+    }
+    return diceArray;
+  }
+
+  function rollDice() {
+    setDice(allNewDice); // get a new dice arrangement
+  }
+
+  // turn random number into a JSX element
+  const diceElements = dice.map((die) => {
+    return <Die value={die} />;
+  });
+
   return (
     <main>
-      <div className='dice-container'>
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={6} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={4} />
-        <Die value={1} />
-      </div>
+      <div className='dice-container'>{diceElements}</div>
+      <button className='roll-dice' onClick={rollDice}>
+        Roll Dice
+      </button>
     </main>
   );
 }
