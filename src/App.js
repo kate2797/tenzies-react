726227ad1ac4e336c80f81976479c0bdfc163be6
kate2797 <katka.zaprazna@icloud.com>
2022-05-27 21:15:46 +0100

    Added a function to roll a die

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
 import Die from "./components/Die";
+import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
 
 export default function App() {
   const [dice, setDice] = React.useState(allNewDice()); // initialise an array of random numbers
 
+  /**
+   * returns an array of die objects
+   */
   function allNewDice() {
     const diceArray = [];
     for (let i = 0; i < 10; i++) {
       let randomNum = Math.floor(Math.random() * 6) + 1;
-      diceArray.push(randomNum);
+      // a die object
+      diceArray.push({
+        value: randomNum,
+        id: nanoid(),
+        isHeld: false,
+      });
     }
     return diceArray;
   }
@@ -17,9 +27,38 @@ export default function App() {
     setDice(allNewDice); // get a new dice arrangement
   }
 
-  // turn random number into a JSX element
+  /**
+   *
+   * modifies the state, flips isHeld of a dice with the given id
+   */
+  function holdDice(id) {
+    // return the new state
+    setDice((prevDice) => {
+      return prevDice.map((die) => {
+        if (die.id === id) {
+          return {
+            ...die,
+            isHeld: !die.isHeld, // modify
+          };
+        } else {
+          return die; // return unmofidied
+        }
+      });
+    });
+  }
+
+  /**
+   * turns a random number into a JSX element
+   */
   const diceElements = dice.map((die) => {
-    return <Die value={die} />;
+    return (
+      <Die
+        key={die.id}
+        value={die.value}
+        isHeld={die.isHeld}
+        holdDice={() => holdDice(die.id)} // pass down the f() with the id
+      />
+    );
   });
 
   return (
